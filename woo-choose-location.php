cbec4ed7c3644b7fc0c7b6151cf83342d16cf3da
Ridwan Arifandi <orangerdigiart@gmail.com>
2024-02-12 19:45:00 +0700

    1.0.3

--- woo-choose-location.php
+++ woo-choose-location.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Woo Choose Location
  * Plugin URI:        https://https://ridwan-arifandi.com/
  * Description:       Special plugin for ranksports.id
- * Version:           1.0.2
+ * Version:           1.0.3
  * Author:            OrangerDev Team
  * Author URI:        https://https://ridwan-arifandi.com//
  * License:           GPL-2.0+
@@ -26,8 +26,8 @@
  */
 
 // If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
-	die;
+if (!defined('WPINC')) {
+  die;
 }
 
 /**
@@ -35,39 +35,41 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'WOO_CHOOSE_LOCATION_VERSION', '1.0.2' );
+define('WOO_CHOOSE_LOCATION_VERSION', '1.0.2');
 
-define( 'WOO_CHOOSE_LOCATION_PATH', plugin_dir_path( __FILE__ ) );
-define( 'WOO_CHOOSE_LOCATION_URI', plugin_dir_url( __FILE__ ) );
+define('WOO_CHOOSE_LOCATION_PATH', plugin_dir_path(__FILE__));
+define('WOO_CHOOSE_LOCATION_URI', plugin_dir_url(__FILE__));
 
-require_once( 'vendor/autoload.php' );
+require_once('vendor/autoload.php');
 
 /**
  * The code that runs during plugin activation.
  * This action is documented in includes/class-woo-choose-location-activator.php
  */
-function activate_woo_choose_location() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-woo-choose-location-activator.php';
-	Woo_Choose_Location_Activator::activate();
+function activate_woo_choose_location()
+{
+  require_once plugin_dir_path(__FILE__) . 'includes/class-woo-choose-location-activator.php';
+  Woo_Choose_Location_Activator::activate();
 }
 
 /**
  * The code that runs during plugin deactivation.
  * This action is documented in includes/class-woo-choose-location-deactivator.php
  */
-function deactivate_woo_choose_location() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-woo-choose-location-deactivator.php';
-	Woo_Choose_Location_Deactivator::deactivate();
+function deactivate_woo_choose_location()
+{
+  require_once plugin_dir_path(__FILE__) . 'includes/class-woo-choose-location-deactivator.php';
+  Woo_Choose_Location_Deactivator::deactivate();
 }
 
-register_activation_hook( __FILE__, 'activate_woo_choose_location' );
-register_deactivation_hook( __FILE__, 'deactivate_woo_choose_location' );
+register_activation_hook(__FILE__, 'activate_woo_choose_location');
+register_deactivation_hook(__FILE__, 'deactivate_woo_choose_location');
 
 /**
  * The core plugin class that is used to define internationalization,
  * admin-specific hooks, and public-facing site hooks.
  */
-require plugin_dir_path( __FILE__ ) . 'includes/class-woo-choose-location.php';
+require plugin_dir_path(__FILE__) . 'includes/class-woo-choose-location.php';
 
 /**
  * Begins execution of the plugin.
@@ -78,10 +80,10 @@ require plugin_dir_path( __FILE__ ) . 'includes/class-woo-choose-location.php';
  *
  * @since    1.0.0
  */
-function run_woo_choose_location() {
+function run_woo_choose_location()
+{
 
-	$plugin = new Woo_Choose_Location();
-	$plugin->run();
-
+  $plugin = new Woo_Choose_Location();
+  $plugin->run();
 }
 run_woo_choose_location();
